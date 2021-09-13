e9ff257127158edb5a5132e8aba40eb8e7696492
Colin <colizu2020@gmail.com>
2021-09-13 09:48:00 -0700

    commit

--- react-interview/src/App.jsx
+++ react-interview/src/App.jsx
@@ -2,52 +2,56 @@ import './App.css';
 import * as React from "react"
 import axios from 'axios';
 
-const Card = (props) => {
-  const {fetchedData} = props
-  console.log("hellowwww: " + fetchedData.results);
-
-  return (
-    <div className="brits">
-      {fetchedData ? <div><p>{fetchedData.results}</p> </div> : null}
-    </div>
-  )
-}
-
 // const returnUserName = (object) => {
 //   return object.results
 // }
 
-const fetchData = async () => {
-  return axios.get('https://randomuser.me/api').then((data) => {
-    return JSON.stringify(data, null, 2)
-  })
-}
 
-const retrieveData = async () => {
-  return axios.get('https://randomuser.me/api').then((data) => {
-    return data
-  })
-  // return 
+// const retrieveData = async () => {
+  //   return axios.get('https://randomuser.me/api').then((data) => {
+    //     return data
+    //   })
+    //   // return 
+    // }
+    
+    const fetchData = (pageNum) => {
+      return axios.get(`https://randomuser.me/api?page=${pageNum}`).then(({data}) => {
+        return data
+      })
+    }
+const getFullUserName = (element) => {
+  // let userInfo
+  const {name: {first, last}} = element
+
+  return `${first} ${last}`
 }
 
 function App() {
   
   const [counter, setCounter] = React.useState(0)
   const [fetched, setFetched] = React.useState(null)
-  const [firstName, setFirstName] = React.useState(null)
+  const [name, setName] = React.useState(null)
+  const [page, setPageNumber] = React.useState(1)
+
+  const fetchUserData = () => {
+    fetchData(page).then((data) => {
+      // const array = [...fetched, ...data.results]
+      // setFetched(array)
+      setFetched(data.results)
+      setName({firstName: data.results[0].name.first, lastName: data.results[0].name.last})
+      setPageNumber(data.info.page + 1)
+    })
+  }
   
   React.useEffect(() => {
-    fetchData().then((data) => {
-      setFetched(data)
-      // setFirstName(data.results)
-    })
+    fetchUserData()
   }, [])
   
-  React.useEffect(() => {
-    retrieveData().then((data) => {
-      setFirstName(data.results)
-    })
-  })
+  // React.useEffect(() => {
+  //   retrieveData().then((data) => {
+  //     setFirstName(data.results)
+  //   })
+  // })
   
   // function getAPI() {
   //   axios.get('https://randomuser.me/api').then(function({data}) {
@@ -59,19 +63,33 @@ function App() {
   //     console.log(err);
   //   })
   // }
+  //data.results[0].picture.medium
 
   return (
     <div className="App">
       <div>
         <button onClick={() => setCounter(counter + 1)}> Click Me </button>
         <button onClick={() => fetchData()}>Fetch Data</button>
+          <button onClick={() => setPageNumber(page + 1)}>Fetch The nExt Ones</button>
         <h1>{counter}</h1>
         <div className="hello">
-          <pre>{fetched}</pre>
         </div>
         <div>
-          HEllo, this is great
-          {firstName}
+          HEllo, this is great: 
+          {fetched ? fetched.map((element, index) => {
+            return (
+              <div key={index}>
+                <img src={element.picture.medium} alt="" />
+                <p> {getFullUserName(element)}</p>
+              </div>
+              
+              )
+            }) : null}
+          {/* {name ? ` ${name.firstName} ${name.lastName}` : null} */}
+        </div>
+        <div>
+        </div>
+        <div>
         </div>
       </div>
     </div>
