6b691a17c5a862269a1d36d6cc9131204a6884f7
czhou578 <colizu2020@gmail.com>
2022-06-25 18:52:55 -0700

    commit

--- Frontendevalquestions/MockJSInterview/script.js
+++ Frontendevalquestions/MockJSInterview/script.js
@@ -1,4 +1,3 @@
-
 // function promisesAllUtility(promises) {
 //   const outputs = []
 //   let settledPromiseCounter = 0;
@@ -26,76 +25,76 @@
 function deepEquals(valueOne, valueTwo) {
   //handling primitives
   if (typeof valueOne !== "object" || typeof valueTwo !== "object") {
-    const isNaNOne = isNaN(valueOne) && typeof valueOne !== 'string'
-    const isNaNTwo = isNaN(valueTwo) && typeof valueTwo !== 'string'
-    
-    return valueOne === valueTwo
+    //check if types aren't objects
+    const isNaNOne = isNaN(valueOne) && typeof valueOne !== "string";
+    const isNaNTwo = isNaN(valueTwo) && typeof valueTwo !== "string";
+
+    if (isNaNOne && isNaNTwo) return true;
+
+    return valueOne === valueTwo;
   }
 
-  if (typeof valueOne !== typeof valueTwo) return false
-  if (valueOne === null && valueTwo === null) return true
-  if (valueOne === null || valueTwo === null) return false
+  if (typeof valueOne !== typeof valueTwo) return false; //if types aren't the same, return false
 
-  if (valueOne === valueTwo) return true
-  
+  if (valueOne === null && valueTwo === null) return true; // if both are null, return true
+
+  if (valueOne === null || valueTwo === null) return false; //if only one of them are null, return true
+
+  if (valueOne === valueTwo) return true; //if both values originally are equal in value and type, then return true
 
   if (Array.isArray(valueOne) && Array.isArray(valueTwo)) {
-    if (valueOne.length !== valueTwo.length) return false
+    //if both are arrays
+    if (valueOne.length !== valueTwo.length) return false;
 
     for (let i = 0; i < valueOne.length; i++) {
-      if (!deepEquals(valueOne[i],valueTwo[i])) return false
+      if (!deepEquals(valueOne[i], valueTwo[i])) return false; //check for nested arrays, if any values aren't equal, then return false
     }
 
-    return true
+    return true;
   }
 
-  if (Array.isArray(valueOne) || Array.isArray(valueTwo)) return false
+  if (Array.isArray(valueOne) || Array.isArray(valueTwo)) return false; // if only one input is an array type, return false
 
-  const valueOneKeys = Object.keys(valueOne)
-  const valueTwoKeys = Object.keys(valueTwo)
+  const valueOneKeys = Object.keys(valueOne); //get key sets of both inputs, since now we know they are objects
+  const valueTwoKeys = Object.keys(valueTwo);
 
-  if (valueOneKeys.length !== valueTwoKeys.length) return false
-  if (!deepEquals(valueOneKeys, valueTwoKeys)) return false //min 38:00 roughly for why this fails
-
-  if (!deepEquals(valueOneKeys, valueTwoKeys)) return false
+  if (valueOneKeys.length !== valueTwoKeys.length) return false; //if the length of keysets are different
+  if (!deepEquals(valueOneKeys, valueTwoKeys)) return false; //min 38:00 roughly for why this fails
 
   for (let i = 0; i < valueOneKeys.length; i++) {
-    const key = valueOneKeys[i]
-    const valueOneValue = valueOne[key]
-    const valueTwoValue = valueTwo[key]
-    if (!deepEquals(valueOneValue, valueTwoValue)) return false  
+    const key = valueOneKeys[i]; //grab index i, which is key in valueOneKeys
+    const valueOneValue = valueOne[key]; //compare the value of this key in value1 and value2, if they are not equal, then return false.
+    const valueTwoValue = valueTwo[key];
+    if (!deepEquals(valueOneValue, valueTwoValue)) return false;
   }
 
-  return true
+  return true;
 
   //Colin's approach: flatten object
   //compare all keys and values of both objects, and if they are the same, then return true
 }
 
-console.log(deepEquals([], {}))
-console.log(deepEquals(null, 4))
-console.log(deepEquals(undefined, undefined))
+console.log(deepEquals([], {}));
+console.log(deepEquals(null, 4));
+console.log(deepEquals(undefined, undefined));
 
 function getTimer(isoDate, timerInfo) {
-  const date = new Date(isoDate)
+  const date = new Date(isoDate);
 
   setInterval(() => {
-    const timeTillDate = date - Date.now()
-    const seconds = Math.floor(timeTillDate / 60)
-    const minutes = Math.floor(seconds/ 60)
-    const hours = Math.floor(minutes / 60)
+    const timeTillDate = date - Date.now();
+    const seconds = Math.floor(timeTillDate / 60);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
 
-    timerInfo.seconds = seconds % 60
-    timerInfo.minutes = minutes % 60
-    timerInfo.hours = hours % 60
-  
+    timerInfo.seconds = seconds % 60;
+    timerInfo.minutes = minutes % 60;
+    timerInfo.hours = hours % 60;
+
     // timerInfo = {
     //   seconds: seconds % 60,
     //   minutes: minutes % 60,
     //   hours
     // }
   }, 500);
-
 }
-
-// const 
